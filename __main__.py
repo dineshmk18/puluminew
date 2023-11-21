8fe29a63ffbdeb82b50c10beebfa31e27fb66beb
dineshmk18 <144545448+dineshmk18@users.noreply.github.com>
2023-11-21 17:05:27 +0530

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,5 @@
+
+from azure_keyvault.keyvault import *     #resource_group, key_vault, secret
+from azure_mysql.mysql import *
+# from pulumi import Input, Output
+# from azure_network.network import resource_group, public_ip, virtual_network, security_group
